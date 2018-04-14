4dfc41b09f91fe509761d534e418ac115e6d6665
Simon Lindgren <simon.g.lindgren@gmail.com>
2018-04-14 16:39:33 +0200

    cmd/server: Added topic/consmer registration

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/julienschmidt/httprouter"
 )
@@ -10,7 +11,10 @@ import (
 func setupRouter(env *Env) *httprouter.Router {
 	router := httprouter.New()
 
-	router.POST("/v1/topic/:name", env.createTopic)
+	router.POST("/v1/topic/:topicName", env.createTopic)
+	router.PUT("/v1/topic/:topicName", env.appendMessage)
+	router.GET("/v1/topic/:topicName/consume", env.addConsumer)
+
 	router.GET("/health", healthCheck)
 
 	return router
@@ -20,8 +24,13 @@ func main() {
 	config := getConfig("8080")
 	env := newEnv()
 
+	go env.broker.Start()
+
 	log.Printf("Starting PLEX_SERVER listening on port: %s\n", config.Port)
 	err := http.ListenAndServe(":"+config.Port, setupRouter(env))
+
+	env.shutdownCh <- 9
+	time.Sleep(1 * time.Second)
 	if err != nil {
 		log.Fatal(err)
 	}
--- cmd/server/topic_handlers.go
+++ cmd/server/topic_handlers.go
@@ -1,18 +1,46 @@
 package main
 
 import (
+	"io/ioutil"
+	"log"
 	"net/http"
 
 	"github.com/CzarSimon/httputil"
+	"github.com/CzarSimon/httputil/query"
 	"github.com/CzarSimon/plex/broker"
+	"github.com/CzarSimon/plex/consumer"
 	"github.com/CzarSimon/plex/pkg"
+	"github.com/CzarSimon/plex/pkg/schema"
 	"github.com/julienschmidt/httprouter"
 )
 
+// createTopic creates a new topic and registers it to the broker.
 func (env *Env) createTopic(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	topic := pkg.NewTopic(ps.ByName("name"))
+	topicName := ps.ByName("topicName")
+	topic := pkg.NewTopic(topicName)
 	err := env.broker.RegisterTopic(topic)
 	if err == broker.ErrTopicAlreadyExists {
+		httputil.SendErr(w, httputil.BadRequest)
+		return
+	}
+	if err != nil {
+		httputil.SendErr(w, httputil.InternalServerError)
+		return
+	}
+	log.Printf("Created new topic: %s\n", topicName)
+	httputil.SendOK(w)
+}
+
+// appendMessage sends a new message over a topic.
+func (env *Env) appendMessage(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	msgBody, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		httputil.SendErr(w, httputil.BadRequest)
+		return
+	}
+	msg := schema.NewMessage(ps.ByName("topicName"), msgBody)
+	err = env.broker.HandleMessage(msg)
+	if err == broker.ErrNoSuchTopic {
 		httputil.SendErr(w, httputil.BadRequest)
 		return
 	}
@@ -23,6 +51,27 @@ func (env *Env) createTopic(w http.ResponseWriter, r *http.Request, ps httproute
 	httputil.SendOK(w)
 }
 
+// addConsumer sets up a consumer handler on a given topic.
+func (env *Env) addConsumer(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	consumerName, err := query.ParseValue(r, "name")
+	if err != nil {
+		httputil.SendErr(w, httputil.BadRequest)
+		return
+	}
+	consumer := consumer.NewLogConsumer(consumerName)
+	err = env.broker.RegisterConsumer(ps.ByName("topicName"), consumer)
+	if err == broker.ErrNoSuchTopic {
+		httputil.SendErr(w, httputil.BadRequest)
+		return
+	}
+	if err != nil {
+		httputil.SendErr(w, httputil.InternalServerError)
+		return
+	}
+	httputil.SendOK(w)
+}
+
+// healthCheck returns a 200 OK if all is well.
 func healthCheck(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	httputil.SendOK(w)
 }
